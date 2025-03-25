11dbf60ee9243c1a50b41d67b18d7bcfbba541f8
Muhammad Rafay <muhammadrafay620@icloud.com>
2025-03-25 06:01:36 +0400

    update foders

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Muhammad Rafay',
+    author_email= 'muhammadrafay620@icloud.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
